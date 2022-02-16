388f0431bf7f15385dee4e4b21aa2de8a0f7864c
Luis Bajo <luis.bajo@hotmail.com>
2022-02-15 19:25:33 -0600

    First commit.

---
+++ src/main/java/com/pnc/dsp/kafkastreamsstackoverflowv1/KafkaStreamsStackoverflowV1Application.java
@@ -0,0 +1,13 @@
+package com.pnc.dsp.kafkastreamsstackoverflowv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaStreamsStackoverflowV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaStreamsStackoverflowV1Application.class, args);
+	}
+
+}
